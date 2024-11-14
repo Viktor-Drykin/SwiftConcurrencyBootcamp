090fa503e6a03609246125ee4ceb97b5cd157a62
Viktor-Drykin <142505425+Viktor-Drykin@users.noreply.github.com>
2024-11-14 13:40:44 +0000

    init project

---
+++ SwiftConcurrencyBootcamp/SwiftConcurrencyBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootcampApp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by DataArt Viktor Drykin on 14.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
